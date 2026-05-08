dcacf27b0efae154446567652c964f4362f8a05a
Bruna Villanova <114350625+brunavillanova@users.noreply.github.com>
2026-05-07 22:21:07 -0300

    api automática produção

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:4000"
+      : "https://sistema-presenca-backend.onrender.com",
 });
 
 export default api;
